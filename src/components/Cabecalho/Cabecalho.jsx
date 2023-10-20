2e2755366edae1ce83f4de59f11a8dad3701311e
nicollyADS <rm552579@fiap.com.br>
2023-10-19 22:19:13 -0300

    taiwlind basics

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,14 +1,27 @@
 import Link from "next/link"
 
+
 export default function Cabecalho (){
     return(
-        <div>
-            <ul>
-                <li><Link href="/produtos/calca">Calca</Link></li>
-                <li><Link href="/produtos/camiseta">Camiseta</Link></li>
-                <li><Link href="/produtos/tenis">Tenis</Link></li>
-            </ul>
-        </div>
+        <header className="bg-slate-800  text-white" >
+            <h1 className="text-center text-3x1 font-extrabold">Produtos</h1>
+            <hr className="bg-red-600 border-red-700" />
+            <nav className="menu">
+                <ul>
+                    <li className="inline" >
+                        <Link href="/produtos/calca">Calca</Link>
+                    </li>
+
+                    <li >
+                        <Link href="/produtos/tenis">Tenis</Link>
+                    </li>
+
+                    <li>
+                        <Link href="/produtos/camiseta">Camiseta</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 
 }
