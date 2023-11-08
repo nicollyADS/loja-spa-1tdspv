1f128c8d8af696f292f37bf722d710d68e22ddbe
nicollyADS <rm552579@fiap.com.br>
2023-11-07 22:01:28 -0300

    começando login com POST

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,79 +1,77 @@
-"use client"
-
+'use client'
 import { useState } from "react"
 
 export default function Login() {
 
-    //use state para comportar o usuario
+    const [msgstatus, setMsgStatus] = useState("")
 
+    //criando um useState para comportar o usuario
     const [usuario, setUsuario] = useState({
-        "email": "",
-        "senha": ""
-    })
-
-    //preenchimento do form
+        "email":"",
+        "senha":""
+    });
+    //função de preenchimento do FORM...
     const handleChange = (e)=>{
-        //Destructuring
+        //Destruturing
         const{name, value} = e.target;
-
-        //Preenchendo o campo utilizando useState com SPREAD + onChange:
-        setUsuario({...usuario,[name]:value})
+        //Prenchendo o campo, utilizando o useState com SPREAD + OnChance:
+        setUsuario({...usuario,[name]:value});
     }
 
-    //validação e envio de dados 
-    const handleSubmit = async (e) => {
+    //Função de validação e ENVIO dos dados.
+    const handleSubmit = async (e)=> {
         e.preventDefault();
-        
-        try{
-            const response = await fetch("http://localhost:3000/api/base/base-users/0",{
+
+
+        try {
+            const response = await fetch("http://localhost:3000/api/base/base-users/0", {
                 method: "POST",
                 headers:{
-                    "content-Type": "application/json"
+                    "Content-Type":"aplication/json"
                 },
                 body: JSON.stringify(usuario)
-            })
-
+            });
             if(response.ok){
-
-                let status = response.json().status;
-
-                if(status){
-                    console.log("USUARIO VALIDADO COM SUCESSO")
+                let status = await response.json();
+                
+                if(status.status == true){
+                    setMsgStatus("Login realizado com SUCESSO!");
                 }else{
-                    console.log("USUARIO OU SENHA INVÁLIDOS")
+                    setMsgStatus("USUARIO OU SENHA INVÁLIDOS!");
+                    
                 }
             }
-
-        }catch(error) {
-
+        } catch (error) {
+            
         }
-
     }
 
+  return (
+    <div>
+        <h1>Informações dos usuarios</h1>
 
-    return (
-      <div>
-          <h1>INFORMAÇÕES DOS USUÁRIOS</h1>
-  
-          <div>
-              <form onSubmit={handleSubmit}>
-                  <fieldset>
-                      <legend>LOGIN</legend>
-                      <div>
-                          <label htmlFor="idEmail">EMAIL:</label>
-                          <input type="email" name="email" id="idEmail" placeholder="Digite o seu EMAIL:" value={usuario.email} onChange={handleChange}  />
-                      </div>
-                      <div>
-                          <label htmlFor="idSenha">SENHA:</label>
-                          <input type="password" name="senha" id="idSenha" placeholder="Digite a sua SENHA:" value={usuario.senha}  onChange={handleChange} />
-                      </div>
-                      <div>
-                          <button>LOGIN</button>
-                      </div>
-                  </fieldset>
-              </form>
-          </div>
-      </div>
-    )
-  }
-  
+            <h2>{msgstatus}</h2>
+
+        <div>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>LOGIN</legend>
+                    <div>
+                        <label htmlFor="idEmail">EMAIL:</label>
+                        <input type="email" name="email" id="idEmail" placeholder="Digite o seu email:"
+                        value={usuario.email} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="idSenha">SENHA:</label>
+                        <input type="password" name="senha" id="idSenha" placeholder="Digite a sua senha:"
+                        value={usuario.senha} onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <button>Login</button>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+  )
+}
